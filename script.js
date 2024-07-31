93b449f46fbb2dfa895a4f6851dac568a5274f40
sachini weragodavidana <sachinisrimali@gmail.com>
2024-07-31 19:29:39 +0200

    modal-game

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModel = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (
+  let i = 0;
+  i < btnsOpenModal.length;
+  i++ ///  excued multipal button
+)
+  btnsOpenModal[i].addEventListener('click', openModel);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// if you want to add event listenser for multiple time use the class list and define the function and cl it
+
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+
+  if (e.key === 'a' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
